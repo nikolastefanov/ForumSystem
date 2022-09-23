d4c138df6f222318fffc9618def50d0a76c2dc7d
nikolastefanov <nikolay_stefanov1965@abv.bg>
2022-09-23 19:57:38 +0300

    CreateApiController

---
+++ ForumSystem/ForumSystem/Controllers/Api/CategoriesApiController.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace ForumSystem.Controllers.Api
+{
+    using ForumSystem.Data;
+    using ForumSystem.Models.Api;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    [Route("api/categories")]
+    public class CategoriesApiController :ControllerBase
+    {
+        private readonly ApplicationDbContext data;
+
+        public CategoriesApiController(ApplicationDbContext data)
+        {
+            this.data = data;
+        }
+
+        [HttpGet]
+        public IEnumerable<CategoriesListModel> AllCategories()
+        {
+           var list=this.data
+                .Categories
+                .Select(c =>new CategoriesListModel
+                   {
+                    Id=c.Id,
+                    ImageUrl=c.ImageUrl,
+                    Name=c.Name
+                   });
+
+            return  list;
+        }
+
+
+    }
+}
---
+++ ForumSystem/ForumSystem/Models/Api/CategoriesListModel.cs
@@ -0,0 +1,24 @@
+﻿
+namespace ForumSystem.Models.Api
+{
+    using ForumSystem.Data.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+
+    public class CategoriesListModel
+    {
+        public int Id { get; init; }
+
+        public string Name { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public IEnumerable<Post> Posts { get; init; }
+
+        public int PostsCount { get; set; }
+
+    }
+}
